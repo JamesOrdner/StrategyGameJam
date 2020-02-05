48e7d7783fb5c18276ec66c3ba6440442b586e8d
JamesOrdner <coasterguy123@gmail.com>
2020-02-04 20:57:29 -0600

    setSprite() sets proper color

--- src/Graphics/GraphicsComponent.cpp
+++ src/Graphics/GraphicsComponent.cpp
@@ -18,8 +18,11 @@ void GraphicsComponent::setRenderer(const Renderer* renderer)
 
 void GraphicsComponent::setSprite(int width, int height, SDL_Color color)
 {
-    Uint32 white = 0xFFFFFFFF;
-    sprite = std::make_unique<Sprite>(renderer->sdlRenderer(), SDL_Rect{ 0, 0, .w = width, .h = height }, white);
+    
+    auto* pixelFormat = SDL_AllocFormat(SDL_PIXELFORMAT_RGBA32);
+    Uint32 mappedColor = SDL_MapRGBA(pixelFormat, color.r, color.g, color.b, color.a);
+    SDL_FreeFormat(pixelFormat);
+    sprite = std::make_unique<Sprite>(renderer->sdlRenderer(), SDL_Rect{ 0, 0, .w = width, .h = height }, mappedColor);
 }
 
 void GraphicsComponent::draw(const SDL_Point& cameraPosition)
--- src/Objects/DrawableObject.cpp
+++ src/Objects/DrawableObject.cpp
@@ -5,5 +5,5 @@ DrawableObject::DrawableObject(const class Engine* engine) :
     GameObject(engine)
 {
     auto* graphicsComponent = createComponent<GraphicsComponent>();
-    graphicsComponent->setSprite(100, 100, SDL_Color{});
+    graphicsComponent->setSprite(100, 100, SDL_Color{ 255, 127, 255, 255 });
 }
