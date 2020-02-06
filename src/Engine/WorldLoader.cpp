a3b33d38864b08444987108fc612b15ef4910b4c
zismaelm <ismaelmarquez@utexas.edu>
2020-02-05 21:18:41 -0600

    Added simple object placement randomizer

--- src/Engine/WorldLoader.cpp
+++ src/Engine/WorldLoader.cpp
@@ -2,22 +2,39 @@
 #include "World.hpp"
 #include "Engine.hpp"
 #include "../Objects/Actor.hpp"
+#include <random>
+
 
 void WorldLoader::createWorld(const Engine* engine, World* world)
 {
+
     createBuildings(world);
     /// createResources();
     /// createScenery();
 
 }
 
+int WorldLoader::getRandomInt()
+{
+    static std::random_device r;
+    static std::default_random_engine randIntGenerator(r());
+    static std::uniform_int_distribution<int> distribution(-255,255);
+
+    return distribution(randIntGenerator);
+}
+
 void WorldLoader::createBuildings(World* world)
 {
+    auto* myObject = world->spawnObject<DrawableObject>();
+    myObject->position.x = 0;
+    myObject->position.y = 0;
+    myObject->setSprite(20,20,SDL_Color{255,255,255,255});
+
     for (int i=0; i<5; i++) {
         auto* myObject = world->spawnObject<DrawableObject>();
-        myObject->position.x = rand()%200;
-        myObject->position.y = rand()%200;
-        myObject->setSprite(20,20,SDL_Color{rand()%255,rand()%255,rand()%255,230});
+        myObject->position.x = getRandomInt();
+        myObject->position.y = getRandomInt();
+        myObject->setSprite(20,20,SDL_Color{(uint8_t)getRandomInt(),(uint8_t)getRandomInt(),(uint8_t)getRandomInt(),255});
     }
 }
 
