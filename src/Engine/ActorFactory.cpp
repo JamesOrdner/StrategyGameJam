d40e943e7316b53d1d4f27f6a5fefd26970c6de5
zismaelm <ismaelmarquez@utexas.edu>
2020-02-09 13:59:58 -0600

    Added unit spawning for additional units

--- src/Engine/ActorFactory.cpp
+++ src/Engine/ActorFactory.cpp
@@ -6,15 +6,39 @@
 #include <SDL_rect.h>
 #include <random>
 
-int ActorFactory::unitCost(PlayerUnit unit, ResourceType resouce)
+int ActorFactory::unitCost(PlayerUnit unit, ResourceType resource)
 {
     switch (unit) {
         case PlayerUnit::Clubman:
-            switch (resouce) {
-                case ResourceType::Wood:    return 1;
+            switch (resource) {
+                case ResourceType::Wood:    return 10;
                 case ResourceType::Iron:    return 0;
                 case ResourceType::Crystal: return 0;
                 case ResourceType::Wolf:    return 0;
+            }
+            break;
+        case PlayerUnit::Ninja:
+            switch (resource) {
+                case ResourceType::Wood:    return 0;
+                case ResourceType::Iron:    return 10;
+                case ResourceType::Crystal: return 0;
+                case ResourceType::Wolf:    return 0;
+            }
+            break;
+        case PlayerUnit::Wizard:
+            switch (resource) {
+                case ResourceType::Wood:    return 0;
+                case ResourceType::Iron:    return 0;
+                case ResourceType::Crystal: return 10;
+                case ResourceType::Wolf:    return 0;
+            }
+            break;
+        case PlayerUnit::Wolf:
+            switch (resource) {
+                case ResourceType::Wood:    return 0;
+                case ResourceType::Iron:    return 0;
+                case ResourceType::Crystal: return 0;
+                case ResourceType::Wolf:    return 10;
             }
             break;
     }
@@ -26,18 +50,43 @@ ResourceType ActorFactory::unitSpawnLocation(PlayerUnit unit)
     switch (unit) {
         case PlayerUnit::Clubman: return ResourceType::Wood;
     }
+    switch (unit) {
+        case PlayerUnit::Ninja: return ResourceType::Iron;
+    }
+    switch (unit) {
+        case PlayerUnit::Wizard: return ResourceType::Crystal;
+    }
+    switch (unit) {
+        case PlayerUnit::Wolf: return ResourceType::Wolf;
+    }
 }
 
 void ActorFactory::spawnUnit(World* world, PlayerUnit unit, const SDL_FPoint& position)
 {
     Actor* a;
+    a = world->spawnObject<Actor>(position);
+    a->setSprite(unitImagePath(unit));
+    /*
     switch (unit) {
         case PlayerUnit::Clubman:
             a = world->spawnObject<Actor>(position);
+            a->setSprite(unitImagePath(unit));
+            break;
+        case PlayerUnit::Ninja:
+            a = world->spawnObject<Actor>(position);
             a->setSprite(50, 35, { 200, 200, 150, 255 });
             break;
+        case PlayerUnit::Wizard:
+            a = world->spawnObject<Actor>(position);
+            a->setSprite(50, 35, { 200, 200, 150, 255 });
+            break;
+        case PlayerUnit::Wolf:
+            a = world->spawnObject<Actor>(position);
+            a->setSprite(50, 35, { 200, 200, 150, 255 });
+            break;
+
     }
-    
+    */
     static std::random_device r;
     static std::default_random_engine rGen(r());
     static std::uniform_real_distribution<float> dir(-1, 1);
@@ -50,10 +99,10 @@ void ActorFactory::spawnUnit(World* world, PlayerUnit unit, const SDL_FPoint& po
 std::string ActorFactory::unitImagePath(PlayerUnit unit)
 {
     switch (unit) {
-        case PlayerUnit::Clubman:       return "/res/textures/world/club.bmp";
-        case PlayerUnit::Ninja:         return "/res/textures/world/ninja.bmp";
-        case PlayerUnit::Wizard:        return "/res/textures/world/wizard.bmp";
-        case PlayerUnit::Wolf:          return "/res/textures/world/wolf.bmp";
+        case PlayerUnit::Clubman:       return "res/textures/world/club.bmp";
+        case PlayerUnit::Ninja:         return "res/textures/world/ninja.bmp";
+        case PlayerUnit::Wizard:        return "res/textures/world/wizard.bmp";
+        case PlayerUnit::Wolf:          return "res/textures/world/wolf.bmp";
         
         /* TODO: Need moar art   
         case PlayerUnit::Archer:        return "/res/textures/world/club.bmp";
